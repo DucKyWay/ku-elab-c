3d434e99eece4eb81febe3dd6489b4e00b3f897b
DucKyWay <niji2549@gmail.com>
2024-12-01 02:03:09 +0700

    on week 1 progress

---
+++ Lecture/test.c
@@ -0,0 +1,36 @@
+// #include <stdio.h> -> Compiler Directive ตัว sth. ที่มี # อยู่ข้างหน้า
+#include <stdio.h>
+#include <unistd.h>
+
+#define CONS_NAJA 0
+
+int main() {
+    int layer; //32 bit
+    int test[10]; // 10 pos
+    printf("Please type your need layer: ");
+    scanf("%d", &layer);
+    
+    /*
+    %d = Decimal
+    %f = Float
+    %c = Character
+    %s = String
+    */
+    printf("%d %c %f %c %s\n", 10, 'A', 2.0, 66, "test");
+    
+    puts("Hello, World! This is Pyramid test!");
+    for(int i=0; i<= layer; i++) {
+        for(int j=layer-i; j>=0; j--) {
+            printf(" ");
+        }
+        for(int j=0; j<=i; j++) {
+            printf("*");
+        }
+        for(int j=0; j<i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/* test */
