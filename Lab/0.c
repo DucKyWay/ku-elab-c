783a93732027abc9a626e18e8af33e9f4d5b8334
DucKyWay <niji2549@gmail.com>
2024-12-21 20:10:28 +0700

    finish super rectangle & nested loop

--- Lab/0.c
+++ Lab/0.c
@@ -1,48 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-
-void pattern(int n) {
-
-    if(n <= 0 || n > 26) {
-        printf("-");
-        return;
-    }
-
-    for(int i = n - 1; i >= -n + 1; i--) {
-        
-        int abs_i = i > 0 ? i : -i; // Absolute I for other side
-
-        for (int j = 0; j < abs_i; j++) { // Hyphen Left
-            printf("--");
-        }
-
-        for (int j = 1; j <= n - abs_i; j++) { // Alp Left
-            printf("%c", 'a' + n - j);
-            if (j != n - abs_i) {
-                printf("-");
-            }
-        }
-
-        for (int j = n - abs_i - 1; j > 0; j--) { // Alp Right
-            printf("-%c", 'a' + n - j);
-        }
-
-        for (int j = 0; j < abs_i; j++) { // Hyphen Right
-            printf("--");
-        }
-
-        printf("\n");
-    }
-
-}
 
 int main() {
-
-    char n_in[100];
-    int n = atoi( fgets(n_in, 100, stdin) );
-    
-    pattern(n);
 
     return 0;
 }
--- Lab/Week_04/04-04_super-rectangle.c
+++ Lab/Week_04/04-04_super-rectangle.c
@@ -4,16 +4,27 @@
 int main() {
 
     char x_in[100], y_in[100];
-    int i, j;
+    int i, j, k;
 
     int x = atoi( fgets( x_in, 100, stdin) );
     int y = atoi( fgets( y_in, 100, stdin) );
 
     for(j = 0; j < y; j++) {
-        for(i = 0; i < x; i++) {
-
-            printf("*");    
-        } 
+        if(j == 0) {
+            for(i = 0; i < x; i++) {
+                printf("*");
+            }
+        } else if(j == y - 1) {
+            for(k = j; k > 0; k--) printf(" ");
+            for(i = 0; i < x; i++) {
+                printf("*");
+        }
+        } else {
+            for(k = j; k > 0; k--) printf(" ");
+            for(i = 0; i < x; i++) {
+                printf("%s", (i == 0 || i == x - 1) ? "*" : " ");
+            } 
+        }   
         printf("\n");
     }
 
