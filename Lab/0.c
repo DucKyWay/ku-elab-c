d76d576aa92f929a72254e2865c0b6a6858b086e
DucKyWay <niji2549@gmail.com>
2025-01-07 15:04:24 +0700

    add week06-07

---
+++ Lab/0.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long *fibo_array(unsigned int n, double *golden_ratio);
+
+int main() {
+    // เขียนฟังก์ชัน main เพื่อทดสอบฟังก์ชัน fibo_array เอง
+
+    
+    return 0;
+}
+
+unsigned long *fibo_array(unsigned int n, double *golden_ratio) {
+
+
+
+}
