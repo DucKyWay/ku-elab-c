08ecd92d093519fe124413bd56289ab9263673fd
DucKyWay <niji2549@gmail.com>
2025-01-07 22:27:55 +0700

    finish araiwa

--- Lab/0.c
+++ Lab/0.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-unsigned long *fibo_array(unsigned int n, double *golden_ratio);
 
 int main() {
-    // เขียนฟังก์ชัน main เพื่อทดสอบฟังก์ชัน fibo_array เอง
 
-    
+    char c, range;
+    char time[10];
+    int i = 0, hr = 0, m = 0;
+
+    printf("Enter a 12-hour time [eg. 12:34 am]: ");
+    while((c = getchar()) != '\n' && c != -1)
+        time[i++] = c;
+    time[i] = '\0';
+
+    for(i = 0; time[i] != ':'; i++) // hr
+        hr = hr * 10 + (time[i] - '0');
+    for(int j = i + 1; time[j] != ' '; j++) // m
+        m = m * 10 + (time[j] - '0');
+    range = time[i + 4]; // range
+
+    hr == 12 ? hr = 0: hr;
+    printf("Equivalent 24-hour time: %02d:%02d", (range == 'p' || range == 'P') ? hr += 12 : hr, m);
+
     return 0;
-}
-
-unsigned long *fibo_array(unsigned int n, double *golden_ratio) {
-
-
-
 }
