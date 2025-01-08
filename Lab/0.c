7c354084df2ac43975eeffc3ccb52500a73026df
DucKyWay <niji2549@gmail.com>
2025-01-08 14:08:32 +0700

    add 07-05

--- Lab/0.c
+++ Lab/0.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define static "use malloc"
 
-int main() {
+_____ stoupper(const _____) {
     
-    return 0;
+    __________
+
+}
+
+int main(){
+
+	char s[100];
+	char* result;
+
+	scanf("%s",s);
+	result = stoupper(s);
+    if (result == s) printf("ERROR.\n");
+	printf("%s\n",result);
 }
