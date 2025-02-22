1c8197c7213c1fabd33522a72e4bec91a1cc12da
DucKyWay <niji2549@gmail.com>
2025-02-22 14:47:41 +0700

    add week 9.5 3.cpp finish

--- Lab/w_0905/3.cpp
+++ Lab/w_0905/3.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+
 using namespace std;
 
 int main() {
+
     int amount, last = 0;
     cin >> amount;
 
-    int *prisoner = malloc(sizeof(int) * amount);
+    vector<int> prisoner(amount);
     for(int i = 0; i < amount; i++) {
         cin >> prisoner[i];
         if(last < prisoner[i])
             last = prisoner[i];
     }
 
-    return ;
-}o9
+    vector<int> check(last + 1, 0);
+    for(int i = 0; i < amount; i++) {  
+        if (prisoner[i] <= last) {
+            check[prisoner[i]] = 1;
+        }
+    }
+
+    for(int i = 1; i <= last; i++) { // เลขคี่
+        if(check[i] == 0 && (i % 2 != 0)) {
+            cout << i << " ";
+        }
+    }
+    cout << "\n";
+
+    for(int i = 1; i <= last; i++) { // เลขคู่
+        if(check[i] == 0 && (i % 2 == 0)) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
--- Lab/w_0905/4.cpp
+++ Lab/w_0905/4.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
+typedef struct Word_ {
+    char word[50];    
+    int amount;
+} Word;
+
 int main() {
+    string input;
+    int i, last_word = 0, find = 0;
+    Word data[20];
+
+    while (true) {
+        cin >> input;
+        if (input == "exit") {
+            break;
+        }
+        
+
+    }
+
+    cout << "Output:" << endl;
     
-    return ;
+
+
+    return 0;
 }
